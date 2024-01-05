2c9d3898b0f32dd1cc409cb0fd372c54b2205e52
mohao <11214622+mohaos@user.noreply.gitee.com>
2024-01-05 15:01:11 +0800

    简单测试用例

--- Util/Equal_test.go
+++ Util/Equal_test.go
@@ -2,19 +2,11 @@ package Util
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
 )
 
 func TestMapEqual(t *testing.T) {
-	//m1 := map[string]string{
-	//	"张三": "1",
-	//}
-	//m2 := map[int]string{
-	//	1: "1",
-	//	2: "2",
-	//}
-	fmt.Println(reflect.DeepEqual(ts{name: "张三", age: 1}, ts2{age: 1, name: "张三"}))
+	fmt.Println(Equal(ts{name: "张三", age: 1}, ts{age: 1, name: "张三"}))
 }
 
 type ts struct {
